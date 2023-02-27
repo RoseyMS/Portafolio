8c4e728baf9161e6738957a936c32e35ccc26fa5
Rosey <rosey.morera@gmail.com>
2023-02-26 19:43:55 -0600

    modificando datos personales e iconos generales

--- validacion.js
+++ validacion.js
@@ -1,1 +1,34 @@
 //Haz tú validación en javascript acá
+var mySpan = document.getElementsByTagName('span')[0];
+var myInput = document.getElementsByTagName('input')[0];
+var cierre = document.getElementsByTagName('span')[1];
+myInput.addEventListener('focus', subirSpan);
+myInput.addEventListener('blur', bajarSpan);
+myInput.addEventListener('keyup', mostrarX);
+cierre.addEventListener('click', borrar_campo);
+function subirSpan() {
+    if (myInput.value == "") {
+        mySpan.className += " foco";
+    } else {
+        mySpan.className = "foco";
+    }
+}
+function bajarSpan() {
+    if (myInput.value == "") {
+        mySpan.className = "";
+    } else {
+        mySpan.className += " relleno";
+    }
+}
+function mostrarX() {
+    if (!myInput.value == "") {
+        cierre.className += " cierre_visible"
+    } else {
+        cierre.className = "cierre"
+    }
+}
+function borrar_campo() {
+    myInput.value = "";
+    mostrarX();
+    bajarSpan();
+}
